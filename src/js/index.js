f3755294eb31119aa05da2529de4d0786e4a0577
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2020-12-09 17:50:49 +0100

    Bind blog basic

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 const axios = require("axios")
+
+const blogDiv = document.getElementById("blog");
+let items = []
 axios.get('http://alemarc.dev/blog').then((response) => {
-    console.log(response.data);
-})
+
+    items = response.data
+}).then(() => {
+    var list = document.getElementById('blog');
+
+    for (const blog of items) {
+        var entry = document.createElement('li');
+        entry.appendChild(document.createTextNode(blog.header));
+        list.appendChild(entry);
+    }
+});
